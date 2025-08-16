10ddff21231f729f8d83606bad8bc37c6620f3b1
Liliya Chobanova <lilia.chobanova@gmail.com>
2025-08-16 11:06:22 +0300

    commiting proj

---
+++ StorySpoil160825/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace StorySpoil160825
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
